60ee5ced2a5fcb6c4e2a15bf97dabeb10a7c2426
vadniks <whatever@fake.some>
2024-03-28 21:23:49 +0300

    Structure & logic

--- src/main.c
+++ src/main.c
@@ -8,7 +8,7 @@
 
 static void assert(bool condition) { if (!condition) abort(); }
 
-static void renderLoop(SDL_Window* window, SDL_GLContext* glContext) {
+static void renderLoop(SDL_Window* nonnull window, SDL_GLContext* nonnull glContext) {
     SDL_Event event;
     int width, height;
 
