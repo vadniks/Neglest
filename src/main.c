3df7850706b4fe08ddbaaf793d1775d581b13595
vadniks <whatever@fake.some>
2024-03-28 21:31:20 +0300

    Structure & logic

--- src/main.c
+++ src/main.c
@@ -26,37 +26,37 @@ static void renderLoop(SDL_Window* nonnull window, SDL_GLContext* nonnull glCont
 }
 
 int main(void) {
-    assert(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS | SDL_INIT_TIMER) == 0);
+    assert(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS | SDL_INIT_TIMER) == 0); {
 
-    SDL_version version;
-    SDL_GetVersion(&version);
-    assert(version.major == 2);
+        SDL_version version;
+        SDL_GetVersion(&version);
+        assert(version.major == 2);
 
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+        SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+        SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+        SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
 
-    SDL_Window* window = SDL_CreateWindow(
-        "title",
-        SDL_WINDOWPOS_CENTERED,
-        SDL_WINDOWPOS_CENTERED,
-        512,
-        512,
-        SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI
-    );
-    assert(window != nullptr);
+        SDL_Window* window = SDL_CreateWindow(
+            "title",
+            SDL_WINDOWPOS_CENTERED,
+            SDL_WINDOWPOS_CENTERED,
+            512,
+            512,
+            SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI
+        );
+        assert(window != nullptr); {
+            SDL_GLContext* glContext = SDL_GL_CreateContext(window); {
+                assert(gladLoadGLLoader((GLADloadproc) &SDL_GL_GetProcAddress) == 1);
+                SDL_GL_SetSwapInterval(1);
 
-    SDL_GLContext* glContext = SDL_GL_CreateContext(window);
-    assert(gladLoadGLLoader((GLADloadproc) &SDL_GL_GetProcAddress) == 1);
-    SDL_GL_SetSwapInterval(1);
-    renderLoop(window, glContext);
-    SDL_GL_DeleteContext(glContext);
+                renderLoop(window, glContext);
 
-    SDL_DestroyWindow(window);
+            } SDL_GL_DeleteContext(glContext);
+        } SDL_DestroyWindow(window);
 
-    SDL_Quit();
+    } SDL_Quit();
     assert(SDL_GetNumAllocations() == 0);
     return 0;
 }
