6434b50fe32488ab64c8d31271847bb5311e1c22
vadniks <whatever@fake.some>
2024-03-31 13:51:28 +0300

    Learning

--- src/main.cpp
+++ src/main.cpp
@@ -72,15 +72,7 @@ static void renderFrame() {
     float vertices[] = {
         0.6f, 0.6f, 0.0f,
         0.6f, -0.3f, 0.0f,
-        -0.3f, 0.6f, 0.0f,
-
-        -0.6f, 0.3f, 0.0f,
-        -0.6f, -0.6f, 0.0f,
-        0.3f, -0.6f, 0.0f
-    };
-    unsigned indices[] = {
-        0, 1, 2,
-        3, 4, 5
+        -0.3f, 0.6f, 0.0f
     };
 
     unsigned vao;
@@ -92,23 +84,14 @@ static void renderFrame() {
     glBindBuffer(GL_ARRAY_BUFFER, vbo);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_DYNAMIC_DRAW);
 
-    unsigned ebo;
-    glGenBuffers(1, &ebo);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_DYNAMIC_DRAW);
-
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
     glEnableVertexAttribArray(0);
 
     glUseProgram(shaderProgram);
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDeleteProgram(shaderProgram);
 
     //
-
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-    glDeleteBuffers(1, &ebo);
-
-    glDeleteProgram(shaderProgram);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glDeleteBuffers(1, &vbo);
