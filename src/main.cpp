ad25979ff5a07f268cfb56edb67dd74cadbcebbe
vadniks <whatever@fake.some>
2024-04-01 23:02:47 +0300

    Learning

--- src/main.cpp
+++ src/main.cpp
@@ -30,11 +30,11 @@ static void renderFrame() {
         #version 330 core
         layout (location = 0) in vec3 position;
         layout (location = 1) in vec3 colorIn;
-        out vec3 colorOut;
+        out vec3 color;
 
         void main() {
             gl_Position = vec4(position, 1.0);
-            colorOut = colorIn;
+            color = colorIn;
         }
     )";
 
@@ -48,11 +48,11 @@ static void renderFrame() {
 
     const char* const fragmentShaderCode = R"(
         #version 330 core
-        in vec3 colorIn;
+        in vec3 color;
         out vec4 colorOut;
 
         void main() {
-            colorOut = vec4(colorIn, 1.0);
+            colorOut = vec4(color, 1.0);
         }
     )";
 
