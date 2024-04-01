de2b28ceef844b1e95bd1da965983871ecd17997
vadniks <whatever@fake.some>
2024-04-01 23:03:12 +0300

    Learning

--- src/main.cpp
+++ src/main.cpp
@@ -73,9 +73,9 @@ static void renderFrame() {
 
     float vertices[] = {
         // positions        // colors
-        0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // bottom right
-        -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // bottom left
-        0.0f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f // top
+        0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+        -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
+        0.0f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f
     };
     unsigned indices[] = {
         0, 1, 2
