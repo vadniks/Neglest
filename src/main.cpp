96bdd0ff5a8553c2a3c34f6add9641bcfb754a59
vadniks <whatever@fake.some>
2024-04-01 21:47:17 +0300

    Learning

--- src/main.cpp
+++ src/main.cpp
@@ -29,9 +29,11 @@ static void renderFrame() {
     const char* const vertexShaderCode = R"(
         #version 330 core
         layout (location = 0) in vec3 position;
+        out vec4 color;
 
         void main() {
             gl_Position = vec4(position.x, position.y, position.z, 1.0);
+            color = vec4(0.5, 0.0, 0.0, 1.0);
         }
     )";
 
@@ -45,10 +47,11 @@ static void renderFrame() {
 
     const char* const fragmentShaderCode = R"(
         #version 330 core
-        out vec4 color;
+        out vec4 finalColor;
+        in vec4 color;
 
         void main() {
-            color = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+            finalColor = color;
         }
     )";
 
