dc277c342629c6027b118accd653e7cc1e6eedf3
vadniks <whatever@fake.some>
2024-04-06 16:47:52 +0300

    Structure & logic

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,3 @@
-/*
- * Neglest - An OpenGL 3D game.
- * Copyright (C) 2024 Vadim Nikolaev (https://github.com/vadniks).
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
 
 #include "defs.hpp"
 #include "Shader.hpp"
@@ -298,8 +281,6 @@ static void renderLoop(SDL_Window* window) {
 int main() {
     assert(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS | SDL_INIT_TIMER) == 0);
     assert(IMG_Init(IMG_INIT_JPG | IMG_INIT_PNG) > 0); {
-
-        SDL_ShowCursor(SDL_DISABLE);
 
         SDL_version version;
         SDL_GetVersion(&version);
