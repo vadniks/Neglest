36c5b6702d4683819d0a8d8a103ebb4144abc8b2
vadniks <whatever@fake.some>
2024-04-09 21:26:32 +0300

    Structure & logic

--- src/main.cpp
+++ src/main.cpp
@@ -143,6 +143,10 @@ int main() {
 
                 glEnable(GL_DEPTH_TEST);
                 glEnable(GL_MULTISAMPLE);
+                glEnable(GL_BLEND);
+
+                glBlendFunc(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA);
+
                 SDL_GL_SetSwapInterval(1);
 
                 renderLoop(window);
