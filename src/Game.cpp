23bd0fc9b89f1fbf0249fc56527bead7484692f9
vadniks <whatever@fake.some>
2024-04-14 21:29:57 +0300

    Game learning

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,21 +1,45 @@
 
 #include "Game.hpp"
+#include "ResourceManager.hpp"
+#include "SpriteRenderer.hpp"
+#include <glm/ext/matrix_clip_space.hpp>
+
+static SpriteRenderer* renderer = nullptr;
 
 Game::Game(unsigned width, unsigned height) :
     state(GameState::GAME_MENU),
     width(width),
     height(height),
     keys()
-{
-
-}
+{}
 
 Game::~Game() {
-
+    delete renderer;
 }
 
 void Game::init() {
+    auto shader = ResourceManager::instance()->loadShader(
+        "shaders/spriteVertex.glsl",
+        "shaders/spriteFragment.glsl",
+        "sprite"
+    );
 
+    glm::mat4 proj = glm::ortho(
+        0.0f,
+        static_cast<float>(width),
+        static_cast<float>(height),
+        0.0f,
+        -1.0f,
+        1.0f
+    );
+
+    shader->use();
+    shader->setValue("image", 0);
+    shader->setValue("projection", proj);
+
+    renderer = new SpriteRenderer(shader);
+
+    ResourceManager::instance()->loadTexture("res/awesomeface.png", true, "face");
 }
 
 void Game::processInput() {
@@ -27,5 +51,11 @@ void Game::update() {
 }
 
 void Game::render() {
-
+    renderer->draw(
+        ResourceManager::instance()->getTexture("face"),
+        glm::vec2(200.0f, 200.0f),
+        glm::vec2(300.0f, 400.0f),
+        45.0f,
+        glm::vec3(0.0f, 1.0f, 0.0f)
+    );
 }
