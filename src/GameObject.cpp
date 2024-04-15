7e9677940076e1dd602d0e82f4fc2c30f8756782
vadniks <whatever@fake.some>
2024-04-15 14:50:25 +0300

    Game learning

---
+++ src/GameObject.cpp
@@ -0,0 +1,25 @@
+
+#include "GameObject.hpp"
+
+GameObject::GameObject(
+    glm::vec2 position,
+    glm::vec2 size,
+    const std::shared_ptr<Texture>& sprite,
+    glm::vec4 color,
+    glm::vec2 velocity
+) :
+    mPosition(position),
+    mSize(size),
+    mVelocity(velocity),
+    mColor(color),
+    mRotation(0.0f),
+    mSolid(false),
+    mDestroyed(false),
+    mSprite(sprite)
+{}
+
+GameObject::~GameObject() = default;
+
+void GameObject::draw(const std::shared_ptr<SpriteRenderer>& renderer) {
+    renderer->draw(mSprite, mPosition, mSize, mRotation, mColor);
+}
---
+++ src/GameObject.hpp
@@ -0,0 +1,27 @@
+
+#pragma once
+
+#include "Texture.hpp"
+#include "SpriteRenderer.hpp"
+#include <glm/glm.hpp>
+#include <memory>
+
+class GameObject {
+private:
+    glm::vec2 mPosition, mSize, mVelocity;
+    glm::vec4 mColor;
+    float mRotation;
+    bool mSolid;
+    bool mDestroyed;
+    std::shared_ptr<Texture> mSprite;
+public:
+    GameObject(
+        glm::vec2 position,
+        glm::vec2 size,
+        const std::shared_ptr<Texture>& sprite,
+        glm::vec4 color = glm::vec4(1.0f),
+        glm::vec2 velocity = glm::vec2(0.0f, 0.0f)
+    );
+    ~GameObject();
+    virtual void draw(const std::shared_ptr<SpriteRenderer>& renderer);
+};
