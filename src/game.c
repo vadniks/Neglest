e1e87b14176d799ea9a9675ad25730b2d0499866
vadniks <whatever@fake.some>
2024-04-30 21:05:58 +0300

    The game

--- src/game.c
+++ src/game.c
@@ -7,18 +7,29 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 
-static int gBlockSize = 0, gWidth = 0, gHeight = 0;
+typedef enum {
+    ENTITY_EMPTY,
+    ENTITY_BOX,
+    ENTITY_PLAYER,
+    ENTITY_ENEMY,
+    ENTITY_GEM
+} Entity;
+
+static int gBlockSize = 0, gWidth = 0, gHeight = 0, gBlocksPerXAxis = 0, gBlocksPerYAxis;
 static CompoundShader* gSpriteShader = nullptr;
 static SpriteRenderer* gSpriteRenderer = nullptr;
 static Texture* gBoxTexture = nullptr;
 static Texture* gPlayerTexture = nullptr;
 static Texture* gEnemyTexture = nullptr;
 static Texture* gGemTexture = nullptr;
+static Entity* gField = nullptr;
 
 void gameInit(int blockSize, int width, int height) {
     gBlockSize = blockSize;
     gWidth = width;
     gHeight = height;
+    gBlocksPerXAxis = gWidth / gBlockSize;
+    gBlocksPerYAxis = gHeight / gBlockSize;
 
     mat4 projection;
     glm_ortho(0.0f, (float) width, (float) height, 0.0f, -1.0f, 1.0f, projection);
@@ -53,6 +64,12 @@ void gameInit(int blockSize, int width, int height) {
     SDL_FreeSurface(gemSurface);
     gGemTexture = textureCreate(xGemSurface->w, xGemSurface->h, xGemSurface->pixels);
     SDL_FreeSurface(xGemSurface);
+
+    gField = SDL_malloc(gBlocksPerXAxis * gBlocksPerYAxis * sizeof(Entity));
+    for (int i = 0; i < gBlocksPerXAxis; i++) {
+        for (int j = 0; j < gBlocksPerYAxis; j++)
+            gField[i * j] = ENTITY_BOX;
+    }
 }
 
 void gameProcessInput(SDL_Keycode* nullable keycode, int deltaTime) {
@@ -64,11 +81,33 @@ void gameUpdate(int deltaTime) {
 }
 
 void gameRender(void) {
-    for (int i = 0; i < gWidth / gBlockSize; i++)
-        for (int j = 0; j < gHeight / gBlockSize; j++)
+    for (int i = 0; i < gBlocksPerXAxis; i++) {
+        for (int j = 0; j < gBlocksPerYAxis; j++) {
+            const Texture* nullable texture = nullptr;
+
+            switch (gField[i * j]) {
+                case ENTITY_EMPTY:
+                    texture = nullptr;
+                    break;
+                case ENTITY_BOX:
+                    texture = gBoxTexture;
+                    break;
+                case ENTITY_PLAYER:
+                    texture = gPlayerTexture;
+                    break;
+                case ENTITY_ENEMY:
+                    texture = gEnemyTexture;
+                    break;
+                case ENTITY_GEM:
+                    texture = gGemTexture;
+                    break;
+            }
+
+            if (texture == nullptr) continue;
+
             spriteRendererDraw(
                 gSpriteRenderer,
-                gBoxTexture,
+                texture,
                 (vec2) {(float) (gBlockSize * i), (float) (gBlockSize * j)},
                 (vec2) {(float) gBlockSize, (float) gBlockSize},
                 0.0f,
@@ -76,13 +115,19 @@ void gameRender(void) {
                 0.0f,
                 (vec4) {1.0f, 1.0f, 1.0f, 1.0f}
             );
+        }
+    }
 }
 
 void gameClean(void) {
     compoundShaderDestroy(gSpriteShader);
+
     spriteRendererDestroy(gSpriteRenderer);
+
     textureDestroy(gBoxTexture);
     textureDestroy(gPlayerTexture);
     textureDestroy(gEnemyTexture);
     textureDestroy(gGemTexture);
+
+    SDL_free(gField);
 }
