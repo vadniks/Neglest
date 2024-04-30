716d9bd57b120fad7e12c90ce8eeabab7f8cd2dd
vadniks <whatever@fake.some>
2024-04-30 12:56:02 +0300

    Smth original

--- src/shapeRenderer.c
+++ src/shapeRenderer.c
@@ -23,7 +23,7 @@ void shapeRendererDestroy(ShapeRenderer* renderer) {
     SDL_free(renderer);
 }
 
-void shapeRendererDrawRectangle(
+void shapeRendererDrawTriangle(
     const ShapeRenderer* renderer,
     const vec2 position,
     const vec2 size,
@@ -49,6 +49,13 @@ void shapeRendererDrawRectangle(
     mat4 model;
     glm_mat4_identity(model);
     glm_translate(model, (vec3) {position[0], position[1], 0.0f});
+
+    glm_translate(model, (vec3) {0.5f * size[0], 0.5f * size[1], 0.0f});
+    glm_rotate(model, glm_rad(rotationX), (vec3) {1.0f, 0.0f, 0.0f});
+    glm_rotate(model, glm_rad(rotationY), (vec3) {0.0f, 1.0f, 0.0f});
+    glm_rotate(model, glm_rad(rotationZ), (vec3) {0.0f, 0.0f, 1.0f});
+    glm_translate(model, (vec3) {-0.5f * size[0], -0.5f * size[1], 0.0f});
+
     glm_scale(model, (vec3) {size[0], size[1], 1.0f});
 
     compoundShaderUse(renderer->shader);
@@ -59,6 +66,18 @@ void shapeRendererDrawRectangle(
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
+}
+
+void shapeRendererDrawRectangle(
+    const ShapeRenderer* renderer,
+    const vec2 position,
+    const vec2 size,
+    float rotationX,
+    float rotationY,
+    float rotationZ,
+    const vec4 color
+) {
+
 }
 
 void shapeRendererDrawCircle(
--- src/shapeRenderer.h
+++ src/shapeRenderer.h
@@ -9,6 +9,15 @@ typedef struct ShapeRenderer ShapeRenderer;
 
 ShapeRenderer* shapeRendererCreate(const CompoundShader* shader);
 void shapeRendererDestroy(ShapeRenderer* renderer);
+void shapeRendererDrawTriangle(
+    const ShapeRenderer* renderer,
+    const vec2 position,
+    const vec2 size,
+    float rotationX,
+    float rotationY,
+    float rotationZ,
+    const vec4 color
+);
 void shapeRendererDrawRectangle(
     const ShapeRenderer* renderer,
     const vec2 position,
