1fc26d1a3902b8a86a794ddc37268adc247e78cd
vadniks <whatever@fake.some>
2024-05-07 17:12:45 +0300

    Procedural generation

--- src/generation.c
+++ src/generation.c
@@ -18,18 +18,65 @@
 
 #include "generation.h"
 #include "game.h"
+#include "list.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <assert.h>
+
+typedef struct {
+    int x, y;
+} Point;
 
 static int gRows = 0, gColumns = 0, gMaxRomWidth = 0, gMaxRoomHeight = 0;
 char** gField = nullptr;
 
 static int xRandom(int max) { return (int) ((float) random() / (float) RAND_MAX * ((float) max + 1.0f)); }
 
-static void createPath(void) {
-//    char current =
+static void shuffleP(Point* array, int n) {
+    int i;
+    for (i = 0; i < n - 1; i++) {
+        int j = i + rand() / (RAND_MAX / (n - i) + 1);
+        Point t = array[j];
+        array[j] = array[i];
+        array[i] = t;
+    }
+}
+
+static bool listContainsPoint(const List* list, const Point* point) {
+    for (int i = 0; i < listSize(list); i++) {
+        const Point* nullable j = listGet(list, i);
+        assert(j != nullptr);
+        if (j->x == point->x && j->y == point->y)
+            return true;
+    }
+    return false;
+}
+
+static void createPath(int x, int y, List* visited) {
+    const int directionsSize = 4;
+    Point directions[directionsSize] = {{0, -1}, {1, 0}, {0, 1}, {-1, 0}};
+    shuffleP(directions, directionsSize);
+
+    for (int i = 0; i < directionsSize; i++) {
+        const int
+            dx = x + directions[i].x,
+            dy = y + directions[i].y;
+
+        if (dx >= 0 && dx < gColumns && dy >= 0 && dy < gRows) {
+            char tile = gField[dy][dx];
+
+            Point* point = SDL_malloc(sizeof *point);
+            *point = (Point) {dx, dy};
+
+            if (tile == 'b' && !listContainsPoint(visited, point)) {
+                listAdd(visited, point);
+                gField[dy][dx] = '.';
+                createPath(dx, dy, visited);
+            } else
+                SDL_free(point);
+        }
+    }
 }
 
 void generationInit(void) {
@@ -53,19 +100,21 @@ void generationInit(void) {
     }
     printf("\n");
 
-//    int entropy;
-//    assert(getentropy(&entropy, sizeof(int)) == 0);
-//    srand(entropy);
+    int entropy;
+    assert(getentropy(&entropy, sizeof(int)) == 0);
+    srand(entropy);
 
-    //
+    List* visited = listCreate(SDL_free);
+    createPath(0, 0, visited);
+    listDestroy(visited);
 
-//    for (int y = 0; y < gRows; y++) {
-//        for (int x = 0; x < gColumns; x++) {
-//            printf("%c", gField[y][x]);
-//        }
-//        printf("\n");
-//    }
-//    printf("\n");
+    for (int y = 0; y < gRows; y++) {
+        for (int x = 0; x < gColumns; x++) {
+            printf("%c", gField[y][x]);
+        }
+        printf("\n");
+    }
+    printf("\n");
 }
 
 void generationClean(void) {
